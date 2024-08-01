fe9d56c65ad2f68c3214e90b06f65d331d7ffe19
Gabriel Behling <102189169+gabrielBehling@users.noreply.github.com>
2024-08-01 10:29:15 -0300

    Create User table in script.sql

--- src/database/script.sql
+++ src/database/script.sql
@@ -1,1 +1,13 @@
+create database capiLoja
+use capiLoja
 
+create table User (
+  id int primary key identiy(1, 1),
+  userName string(50) unique not null,
+  name string(50) not null,
+  email string(100) unique not null,
+  passwordHash string(16) not null,
+  phone string(20) unique,
+  isSeller boolean not null default false,
+  birthDate date
+)
