3f7a96626cc51b4d677f295632c60f5217a8d12e
gabrielBehling <gabrielbehling1@gmail.com>
2024-08-01 23:04:52 -0300

    Add trigger to auto update total value of purchase

--- src/database/script.sql
+++ src/database/script.sql
@@ -2,55 +2,80 @@ create database capiLoja
 use capiLoja
 go
 
-create table User (
-  idUser int primary key identiy(1, 1),
-  userName varchar(50) unique not null,
-  name varchar(50) not null,
-  email varchar(100) unique not null,
-  passwordHash varchar(16) not null,
-  phone varchar(20) unique,
-  isSeller boolean not null default false,
-  birthDate date
+create table Client (
+    idClient int primary key identity(1, 1),
+    UserName varchar(50) unique not null,
+    ClientName varchar(50) not null,
+    email varchar(100) unique not null,
+    passwordHash varchar(16) not null,
+    phone varchar(20) unique,
+    isSeller bit not null default 0,
+    birthDate date
 )
 go
 
-create table Product (
-  idProduct int primary key identity(1, 1),
-  name varchar(50) not null,
-  description varchar(200) not null,
-  price decimal(10, 2) not null,
-  idVendedor int,
-  foreign key (idVendedor) references User (idUser)
+create table Item (
+    idItem int primary key identity(1, 1),
+    itemName varchar(50) not null,
+    itemDescription varchar(200) not null,
+    price decimal(10, 2) not null,
+    idVendedor int,
+    foreign key (idVendedor) references Client (idClient)
 )
 go
 
 create table Avaliation (
-  idAvaliation int primary key identity(1, 1),
-  score tinyint not null,
-  comment varchar(200),
-  idUser int,
-  idProduct int,
-  foreign key (idUser) references User (idUser),
-  foreign key (idProduct) references Product (idProduct)
+    idAvaliation int primary key identity(1, 1),
+    score tinyint not null,
+    comment varchar(200),
+    idClient int,
+    idItem int,
+    foreign key (idClient) references Client (idClient),
+    foreign key (idItem) references Item (idItem)
 )
 go
-  
+
 create table Purchase (
-  idPurchase int primary key identity(1, 1),
-  purchaseDatetime datetime not null,
-  purchaseArrival datetime,
-  totalValue decimal(10, 2) not null,
-  idUser int,
-  foreign key (idUser) references User (idUser)
+    idPurchase int primary key identity(1, 1),
+    purchaseDatetime datetime not null,
+    purchaseArrival datetime,
+    totalValue decimal(10, 2) not null,
+    idClient int,
+    foreign key (idClient) references Client (idClient)
 )
 go
-  
+
 create table Sale (
-  idSale int primary key identity(1, 1),
-  quantity int not null default 1,
-  idProduct int,
-  idPurchase int,
-  foreign key (idProduct) references Product (idProduct),
-  foreign key (idPurchase) references Purchase (idPurchase)
+    idSale int primary key identity(1, 1),
+    quantity int not null default 1,
+    idItem int,
+    idPurchase int,
+    foreign key (idItem) references Item (idItem),
+    foreign key (idPurchase) references Purchase (idPurchase)
 )
+go
+
+create trigger TRG_updateTotal
+on Sale
+after insert, update, delete
+as
+    declare
+    @idPurchase int,
+    @oldQuantity int,
+    @newQuantity int,
+    @price decimal(10, 2)
+
+    select @idPurchase = d.idPurchase, @oldQuantity = d.quantity, @price = i.price
+    from DELETED d
+    inner join Item i on d.idItem = i.idItem
+
+    select @idPurchase = ISNULL(ins.idPurchase, @idPurchase),
+	@price = ISNULL(i.price, @price),
+	@newQuantity = ins.quantity
+    from inserted ins
+    inner join Item i on ins.idItem = i.idItem
+
+    update Purchase
+    set totalValue = totalValue - (ISNULL(@oldQuantity, 0) * @price) + (ISNULL(@newQuantity, 0) * @price)
+    where idPurchase = @idPurchase
 go
